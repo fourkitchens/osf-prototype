ca0b24fd0b4a63e2a3b5e76576d9da245c7e5cdf
Aaron Stanush <aaron@fourkitchens.com>
2014-01-23 18:13:13 -0600

    Moving Walls: Adding 2nd subnav menu and rest of page content.

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,9 @@
 var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+    menuLeft2 = document.getElementById( 'cbp-spmenu-s2' ),
     showLeft = document.getElementById( 'showLeft' ),
+    showLeft2 = document.getElementById( 'showLeft2' ),
     closeLeft = document.getElementById( 'closeLeft' ),
+    closeLeft2 = document.getElementById( 'closeLeft2' ),
     body = document.body;
  
 showLeft.onclick = function() {
@@ -9,20 +12,39 @@ showLeft.onclick = function() {
     disableOther( 'showLeft' );
 };
  
-function disableOther( button ) {
-    if( button !== 'showLeft' ) {
-        classie.toggle( showLeft, 'disabled' );
-    }
-};
-
 closeLeft.onclick = function() {
     classie.toggle( this, 'active' );
     classie.toggle( menuLeft, 'cbp-spmenu-open' );
     disableOther( 'closeLeft' );
 };
- 
+
+// Menu 2
+
+showLeft2.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuLeft2, 'cbp-spmenu-open' );
+    disableOther( 'showLeft2' );
+};
+
+closeLeft2.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( menuLeft2, 'cbp-spmenu-open' );
+    disableOther( 'closeLeft2' );
+};
+
+// Close
+
 function disableOther( button ) {
+    if( button !== 'showLeft' ) {
+        classie.toggle( showLeft, 'disabled' );
+    }
     if( button !== 'closeLeft' ) {
         classie.toggle( closeLeft, 'disabled' );
+    }
+    if( button !== 'showLeft2' ) {
+        classie.toggle( showLeft2, 'disabled' );
+    }
+    if( button !== 'closeLeft2' ) {
+        classie.toggle( closeLeft2, 'disabled' );
     }
 }
